23a471f45db619df893981fcaa4f94741ddb47dd
Алишер Борангазиев <@local.itstep>
2019-05-03 21:29:13 +0600

    Добавьте файлы проекта.

---
+++ Binary trees/Source.cpp
@@ -0,0 +1,19 @@
+
+#include<string>
+#include"node.h"
+
+
+void main()
+{
+	tree_<int, string> tr;
+	tr.insert(7, "seven");
+	tr.insert(0, "zero");
+	tr.insert(29, "twenty nine");
+	tr.insert(83, "eighty three");
+	tr.insert(-15, "minus fifteen");
+
+
+
+	system("pause");
+
+}
